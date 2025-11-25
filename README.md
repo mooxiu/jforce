d7abd24a89a0488fe5324fd2d3117785a4c5cc5c
Muyao Xiao <monetshaw@outlook.com>
2025-11-25 15:02:09 +0900

    load pjrt api

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # XLA_GLUE
 
+## Dependencies
+- compile time
+```sh
+cp ${XLA}/xla/pjrt/c/*.h ./third_party/headers/
+```
+- library
+
+seems we have build by ourseleves:
+```sh
+# under xla project
+bazel build //xla/pjrt/c:pjrt_c_api_cpu_plugin.so
+```
+
+
 ## Compile
 
 ```sh
@@ -7,3 +21,12 @@ mkdir build && cd build
 cmake -G Ninja ./..
 ninja
 ```
+
+## Runtime
+
+Runtime will do following this:
+- Compile the StableHLO to executable (PJRT)
+- Put the argumnt vectors in a buffer (PJRT)
+- Get back the result (PJRT)
+
+Referencing: https://github.com/EnzymeAD/Reactant.jl/blob/1e62b5988e90deb790532c54823dbf41893dbc25/deps/ReactantExtra/API.cpp#L1243
--- src/runtime.cpp
+++ src/runtime.cpp
@@ -1,8 +1,20 @@
+#include <cstddef>
 #include <cstdio>
 #include <iostream>
 #include <ostream>
 #include <regex>
 #include <string>
+#include <dlfcn.h>
+#include "../third_party/headers/pjrt_c_api.h"
+
+std::string getPluginPath() {
+    // DEFAULT_PJRT_PLUGIN_PATH should be defined in CMake
+    #ifdef DEFAULT_PJRT_PLUGIN_PATH
+        return DEFAULT_PJRT_PLUGIN_PATH;
+    #else
+        throw std::runtime_error("PJRT plugin path not found. Please set PJRT_PLUGIN_PATH.");
+    #endif
+}
 
 // Source - https://stackoverflow.com/a
 // Posted by Czarek Tomczak, modified by community. See post 'Timeline' for change history
@@ -32,6 +44,7 @@ auto addition_op_gen = [](int size) {
 };
 
 void execute_stableHLO(std::string stableHLO_func, char** args) {
+    
     static const std::regex addition_op_regexp(R"(stablehlo.add)");
     if (std::regex_match(stableHLO_func, addition_op_regexp)) {
         std::cout << "This is a addition operation" << std::endl;
@@ -40,16 +53,53 @@ void execute_stableHLO(std::string stableHLO_func, char** args) {
     }
 }
 
+void execute() {
+    auto handle_ = dlopen(getPluginPath().c_str(), RTLD_LAZY | RTLD_LOCAL);
+    if (!handle_) {
+        std::cerr << "error loading plugin: " << dlerror() << std::endl;
+        return;
+    }
+
+    void* get_api_sym = dlsym(handle_, "GetPjrtApi");
+    if (!get_api_sym) {
+        std::cerr << "error finding GetPjrtApi: " << dlerror() << std::endl;
+        return;
+    }
+
+    // 3. 将符号转换为函数指针类型
+    using GetPjrtApiFn = PJRT_Api* (*)();
+    auto get_api_fn = reinterpret_cast<GetPjrtApiFn>(get_api_sym);
+    
+    auto api = get_api_fn();
+    if (api) {
+        std::cout << "[LOG] API loaded successfully" << std::endl;
+        return;
+    } 
+
+    PJRT_Client_Create_Args args;
+    PJRT_Client* client = nullptr;
+    args.client = client;
+    
+    PJRT_Error* error = api->PJRT_Client_Create(&args);
+    if (error) {
+        std::cerr << "[Err] error creating client" << std::endl;
+        return;
+    }
+    
+    return;
+}
+
 extern "C" void launch_kernel(void* a_ptr, void* b_ptr, void* out_ptr, long n) {
     float* a_float_ptr = static_cast<float*>(a_ptr);
     float* b_float_ptr = static_cast<float*>(b_ptr);
     float* o_float_ptr = static_cast<float*>(out_ptr);
 
-    // 未来这里就是：StableHLO → PJRT → GPU
+    /** 
+        Start: testing Fortran could call runtime written in CPP
+    */
     for (long i = 0; i < n; ++i) {
         o_float_ptr[i] = a_float_ptr[i] + b_float_ptr[i];
     }
-
     auto print_vec = [](float* vec, int len, std::string name) {
         std::cout << "The element of " << name << ": ";
         for (int i = 0; i < len; i++) {
@@ -57,10 +107,19 @@ extern "C" void launch_kernel(void* a_ptr, void* b_ptr, void* out_ptr, long n) {
         } 
         std::cout << std::endl;
     };
-
-    addition_op_gen(n);
-    
     print_vec(a_float_ptr, n, "a_vec");
     print_vec(b_float_ptr, n, "b_vec");
     print_vec(o_float_ptr, n, "c_vec");
+    /**
+        End: testing Fortran could call runtime written in CPP
+     */
+
+
+    /**
+        Start: Using real addition through XLA
+     */
+    execute();
+    /**
+        End: Using real addition through XLA
+     */
 }
