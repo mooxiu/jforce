873a11c682fde7a85c26a1b01da10cbaf38f5ced
Muyao Xiao <monetshaw@outlook.com>
2026-05-06 12:31:45 +0900

    outline affine pass

--- src/transform/Passes.h
+++ src/transform/Passes.h
@@ -18,6 +18,7 @@ namespace xla_jit {
   std::unique_ptr<mlir::Pass> createFIRLoadToAffineLoadPass();
   std::unique_ptr<mlir::Pass> createFirLICMPass();  
   std::unique_ptr<mlir::Pass> createArithRaisingPass();
+  std::unique_ptr<mlir::Pass> createOutlineAffinePass();
 
   void registerAnnotatePass();
   void registerAliasingPass();
@@ -32,6 +33,7 @@ namespace xla_jit {
   void registerFIRLoadToAffineLoadPass();
   void registerFirLICMPass();
   void registerArithRaisingPass();
+  void registerOutlineAffinePass();
 
   void registerAllJitPasses();
 }
