18ae441a780093bb77b3bd8a7052f9393106d35d
Muyao Xiao <monetshaw@outlook.com>
2026-05-10 11:39:08 +0900

    update cleanFIRLoopPass

--- src/transform/CleanFIRLoopPass.cpp
+++ src/transform/CleanFIRLoopPass.cpp
@@ -21,179 +21,113 @@
 using namespace mlir;
 
 namespace {
-
-static void debugging(func::FuncOp fop) {
-  fop.walk(
-      [&](fir::DoLoopOp dlOp) { 
-      // dlOp.getOperation()->print(llvm::dbgs()); 
-      auto terminator = dlOp.getBody()->getTerminator();
-      terminator->print(llvm::dbgs());
-    });
+enum StoreType {
+  FIR_STORE,
+  MEMREF_STORE
 };
 
+struct IterArgsBuffer {
+  StoreType StoreInst;
+  Value StoreMemref;
+};
 
-static void cleanIterArgs(mlir::OpBuilder& opBuilder, func::FuncOp fop) {
-  llvm::SmallVector<fir::DoLoopOp, 4> doLoopOps;
-  fop.walk([&](fir::DoLoopOp dlOp){
-    if (dlOp.getNumRegionIterArgs() > 0) {
-      assert(dlOp.getNumRegionIterArgs() == 1);
-      doLoopOps.push_back(dlOp);
-    }
-  });
-
-  for (auto dlOp: doLoopOps) {
-    assert(dlOp.getRegionIterArgs().size() == 1);
-    // The real induction varaible IV
-    auto loopIV = dlOp.getInductionVar();
-
-    // Return %arg10 in `fir.do_loop ... iter_args(%arg10 = %9)`
-    auto iterArg = dlOp.getRegionIterArgs()[0]; 
-    Value storedIterArg;
-
-    llvm::SmallVector<Operation*> toDelete; 
-    dlOp.walk([&](Operation* op){
-      llvm::TypeSwitch<Operation *>(op)
-        .Case([&](fir::StoreOp storeOp){
-          // storedIterArg is assigned with %0#0 in `fir.store %arg10 to %0#0`
-          if (storeOp.getValue() == iterArg) {
-            storedIterArg = storeOp.getMemref();
-            toDelete.push_back(storeOp);
-          }
-        })
-        .Case([&](fir::LoadOp loadOp){
-          if (storedIterArg != nullptr && loadOp.getMemref() == storedIterArg){
-            // %0#0 is where the iter_arg is stored to:
-            // %12 = fir.load %0#0 : !fir.ref<i32>
-            // We need to replace this with %12 = fir.convert IV: (index) -> get%12Type
-            opBuilder.setInsertionPoint(loadOp); 
-            auto convertOp = fir::ConvertOp::create(
-              opBuilder, 
-              loadOp.getLoc(), 
-              loadOp.getResult().getType(),
-              {loopIV}, 
-              {}
-            );
-            loadOp.getResult().replaceAllUsesWith(convertOp.getResult());
-            toDelete.push_back(loadOp);
-          }
-        });
-    });
-    for (auto* op: toDelete) {
-      op->erase();
-    }
-  }
-
-  return;
-}
-
+IterArgsBuffer buffer;
 
 // example: 
 //  %8 = fir.do_loop %arg9 = %c1 to %c4 step %c1 iter_args(%arg10 = %7) -> (i32) {
 // Inducation Variable (IV): %arg9 which we should use.
 // Iteration Arg: %arg10 which would be loaded and stored, we should avoid use it (them).
-static void cleanIterArgsNew(mlir::OpBuilder& opBuilder, func::FuncOp fop) {
-  llvm::SmallVector<fir::DoLoopOp> doLoops;
-  fop.walk([&](fir::DoLoopOp doLoop){doLoops.push_back(doLoop);});    
-  
+static void replaceIterArgs(mlir::OpBuilder& opBuilder, fir::DoLoopOp doLoop) {
   llvm::SmallVector<Operation*> toDeleteOps;
-  for (auto doLoop: doLoops) {
-    assert(doLoop.getRegionIterArgs().size() == 1);
-    auto loopIV = doLoop.getInductionVar();
-    auto iterArg = doLoop.getRegionIterArgs()[0]; 
+  auto loopIV = doLoop.getInductionVar();
+  assert(doLoop.getRegionIterArgs().size() == 1);
+  auto iterArg = doLoop.getRegionIterArgs()[0];
 
-    doLoop.walk([&](fir::StoreOp storeOp){
-      if (storeOp.getValue() == iterArg){
-        opBuilder.setInsertionPoint(storeOp);
-        auto convertOp = fir::ConvertOp::create(opBuilder, storeOp.getLoc(), iterArg.getType(), loopIV, {});
-        fir::StoreOp::create(opBuilder, storeOp.getLoc(), convertOp.getResult(), storeOp.getMemref());  
-        toDeleteOps.push_back(storeOp);
-      }
-      return;
-    });
+  doLoop.walk([&](fir::StoreOp storeOp){
+    if (storeOp.getValue() == iterArg){
+      buffer.StoreInst = StoreType::FIR_STORE;
+      buffer.StoreMemref = storeOp.getMemref();
 
-    doLoop.walk([&](memref::StoreOp storeOp){
-      if (storeOp.getValue() == iterArg){
-        opBuilder.setInsertionPoint(storeOp);
-        auto convertOp = fir::ConvertOp::create(opBuilder, storeOp.getLoc(), iterArg.getType(), loopIV, {});
-        memref::StoreOp::create(opBuilder, storeOp.getLoc(), convertOp.getResult(), storeOp.getMemref(), storeOp.getIndices());
-        toDeleteOps.push_back(storeOp);
-      }
-      return;
-    });
-  }
+      opBuilder.setInsertionPoint(storeOp);
+      auto convertOp = fir::ConvertOp::create(opBuilder, storeOp.getLoc(), iterArg.getType(), loopIV, {});
+      fir::StoreOp::create(opBuilder, storeOp.getLoc(), convertOp.getResult(), storeOp.getMemref());  
+      toDeleteOps.push_back(storeOp);
+    }
+    return;
+  });
+
+  doLoop.walk([&](memref::StoreOp storeOp){
+    if (storeOp.getValue() == iterArg){
+      buffer.StoreInst = StoreType::MEMREF_STORE;
+      buffer.StoreMemref = storeOp.getMemref();
+
+      opBuilder.setInsertionPoint(storeOp);
+      auto convertOp = fir::ConvertOp::create(opBuilder, storeOp.getLoc(), iterArg.getType(), loopIV, {});
+      memref::StoreOp::create(opBuilder, storeOp.getLoc(), convertOp.getResult(), storeOp.getMemref(), storeOp.getIndices());
+      toDeleteOps.push_back(storeOp);
+    }
+    return;
+  });
 
   for (auto* toDeleteOp: toDeleteOps){
     toDeleteOp->erase();
   }
 }
 
+static void replaceLoopSignature(OpBuilder& opBuilder, fir::DoLoopOp doLoop){
+  opBuilder.setInsertionPoint(doLoop);
+  mlir::NamedAttrList filteredAttrs(doLoop->getAttrs());
+  filteredAttrs.erase("unordered");
+  filteredAttrs.erase("finalCountValue");
+  filteredAttrs.erase(doLoop.getOperandSegmentSizeAttr());
+  auto newDoLoop = fir::DoLoopOp::create(
+    opBuilder, 
+    doLoop.getLoc(), 
+    doLoop.getLowerBound(), 
+    doLoop.getUpperBound(),
+    doLoop.getStep(),
+    doLoop.getUnordered().has_value()? doLoop.getUnordered().value(): false,
+    /*finalCountValue*/ doLoop.getFinalValue().has_value(),
+    /*ArgIters*/ {},
+    /*reduceOperands*/ {},
+    /*reduceAttrs=*/ {},
+    filteredAttrs
+  );
+  doLoop.getInductionVar().replaceAllUsesWith(newDoLoop.getInductionVar());
+  auto newBlock = newDoLoop.getBody(); 
+  auto existingTerminator = newBlock->getTerminator();
+  newBlock->getOperations().splice(
+    Block::iterator(existingTerminator),
+    doLoop.getBody()->getOperations(),
+    doLoop.getBody()->getOperations().begin(),
+    Block::iterator(doLoop.getBody()->getTerminator())
+  );
 
-// Jforce suppose the loop in included in.
-// Should consider whether this is a first private.
-static void cleanLoopResult(OpBuilder& opBuilder, func::FuncOp fop) {
-  llvm::SmallVector<fir::DoLoopOp, 4> oldDoLoopOps;
-  fop.walk([&](fir::DoLoopOp dlOp){
-    // Delete the result and reference to the do loop result
-    if (dlOp.getNumResults() == 0) {
-      return;
-    }
-    oldDoLoopOps.push_back(dlOp);
-  });
-
-  for (auto dlOp: oldDoLoopOps) {
-    Value res = dlOp.getResult(0);
-
-
-    llvm::SmallVector<Operation*> toDelete;
-    auto users = res.getUsers();
-    for (auto user: users) {
-      auto storeOp = llvm::dyn_cast<fir::StoreOp>(user);
-      assert(storeOp);
-      toDelete.push_back(storeOp);
-    }
-    for (auto op: toDelete) {
-      op->erase();
-    }
-
-    // Delete the return valus of do loop
-    opBuilder.setInsertionPoint(dlOp);
-    mlir::NamedAttrList filteredAttrs(dlOp->getAttrs());
-    filteredAttrs.erase("unordered");
-    filteredAttrs.erase("finalCountValue");
-    filteredAttrs.erase(dlOp.getOperandSegmentSizeAttr());
-    auto newDoLoopOp = fir::DoLoopOp::create(
-      opBuilder, 
-      dlOp.getLoc(), 
-      dlOp.getLowerBound(), 
-      dlOp.getUpperBound(),
-      dlOp.getStep(),
-      dlOp.getUnordered().has_value()? dlOp.getUnordered().value(): false,
-      /*finalCountValue*/false,
-      /*ArgIters*/ {},
-      {},
-      {},
-      filteredAttrs
-    );
-
-
-    dlOp.getInductionVar().replaceAllUsesWith(newDoLoopOp.getInductionVar());
-
-
-    auto newBlock = newDoLoopOp.getBody(); 
-    auto existingTerminator = newBlock->getTerminator();
-    newBlock->getOperations().splice(
-      Block::iterator(existingTerminator),
-      dlOp.getBody()->getOperations(),
-      dlOp.getBody()->getOperations().begin(),
-      Block::iterator(dlOp.getBody()->getTerminator())
-    );
-
-    assert(dlOp.use_empty());
-    dlOp.erase();
+  auto oldTerminator = cast<fir::ResultOp>(doLoop.getBody()->getTerminator());
+  assert(oldTerminator.getNumOperands() == 1);
+  assert(doLoop.getNumResults() == 1);
+  Value newResult;
+  switch (buffer.StoreInst) {
+    case StoreType::FIR_STORE:
+      opBuilder.setInsertionPoint(existingTerminator);
+      fir::StoreOp::create(opBuilder, oldTerminator.getLoc(), oldTerminator.getOperand(0), buffer.StoreMemref);
+      opBuilder.setInsertionPointAfter(newDoLoop);
+      newResult = fir::LoadOp::create(opBuilder, newDoLoop.getLoc(), buffer.StoreMemref).getResult();
+      break;
+    case StoreType::MEMREF_STORE:
+      opBuilder.setInsertionPoint(existingTerminator);
+      memref::StoreOp::create(opBuilder, oldTerminator.getLoc(), oldTerminator.getOperand(0), buffer.StoreMemref, {});
+      opBuilder.setInsertionPointAfter(newDoLoop);
+      newResult = memref::LoadOp::create(opBuilder, newDoLoop.getLoc(), buffer.StoreMemref, {}).getResult();
+      break;
+    default:
+      break;
   }
-
-  return;
-};
+  doLoop.getResult(0).replaceAllUsesWith(newResult);
+  oldTerminator.erase();
+  assert(doLoop.use_empty());
+  doLoop.erase();
+}
 
 struct CleanFIRLoopPass
     : public PassWrapper<CleanFIRLoopPass, OperationPass<func::FuncOp>> {
@@ -208,8 +142,16 @@ struct CleanFIRLoopPass
   void runOnOperation() override {
     auto funcOp = getOperation();
     OpBuilder opBuilder(funcOp.getContext());
-    cleanIterArgsNew(opBuilder, funcOp);
-    // cleanLoopResult(opBuilder, funcOp);
+    
+    llvm::SmallVector<fir::DoLoopOp> doLoops;
+    funcOp.walk([&](fir::DoLoopOp doLoop){doLoops.push_back(doLoop);});    
+    for (auto doLoop: doLoops) {
+      if (doLoop.getNumRegionIterArgs() > 0) {
+        assert(doLoop.getNumRegionIterArgs() == 1 && "Currently only deal with one iterarg");
+        replaceIterArgs(opBuilder, doLoop);
+        replaceLoopSignature(opBuilder, doLoop);
+      }
+    } 
   };
 };
 } // namespace
