13f8d1bc148342ec07cf88b325b756eabf15271e
Muyao Xiao <monetshaw@outlook.com>
2026-05-20 14:09:01 +0900

    Dev/jforce general multidim (#30)
    * [tests] remove unused test mlirs
    
    * [tests] 2d example
    
    * [pass] fix CleanFIROpsPass
    
    * [pass] fix CleanFIROpsPass: only outline outter
    
    * [chores] ignore tests/tmp.mlir
    
    * [pass] fix OutlineAffinePass
    
    * [pass] add loopSinkPass
    
    * [pass] fix loopSinkPass
    
    * [pass] fix loopSinkPass, adding lowering pipeline

--- src/transform/Passes.h
+++ src/transform/Passes.h
@@ -23,6 +23,7 @@ namespace xla_jit {
   std::unique_ptr<mlir::Pass> createCleanFIRLoadPass();
   std::unique_ptr<mlir::Pass> createCleanFIROpsPass();
   std::unique_ptr<mlir::Pass> createOptimizeMemOpsPass();
+  std::unique_ptr<mlir::Pass> createLoopSinkingPass();
 
   void registerAnnotatePass();
   void registerAliasingPass();
@@ -42,6 +43,7 @@ namespace xla_jit {
   void registerCleanFIROpsPass();
   void registerOptimizeMemOpsPass();
   void registerAllJitPasses();
+  void registerLoopSinkingPass();
 }
 
 #endif
