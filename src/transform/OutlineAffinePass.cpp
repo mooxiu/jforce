9febfa1223c11e61d064b3fbf9347e4c8b4dc21c
Muyao Xiao <monetshaw@outlook.com>
2026-05-20 22:05:23 +0900

    [pass] fix OutlineAffinePass for affine loops with return values

--- src/transform/OutlineAffinePass.cpp
+++ src/transform/OutlineAffinePass.cpp
@@ -20,6 +20,9 @@
 #include "mlir/Support/LLVM.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "mlir/Transforms/RegionUtils.h"
+#include "support/utilities.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/DenseSet.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SetVector.h"
 #include "llvm/ADT/SmallVector.h"
@@ -31,6 +34,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <memory>
+#include <string>
 #include <utility>
 
 using namespace mlir;
@@ -43,97 +47,75 @@ template <typename LoopType>
 struct AffineLoopsToOutline : public OpRewritePattern<LoopType> {
   using OpRewritePattern<LoopType>::OpRewritePattern;
 
-  static llvm::SetVector<Value> getOutDefinedVals(LoopType loopOp) {
+  static llvm::SetVector<Value> getPredefinedVals(LoopType loopOp) {
     // Collect all values defined outside of the affineOp itself.
-    llvm::SetVector<Value> outDefinedVals;
+    llvm::SetVector<Value> preloopDefinedVals;
 
     // Values defined outside of forOp used in forOp region.
-    mlir::getUsedValuesDefinedAbove({loopOp.getRegion()}, outDefinedVals);
+    mlir::getUsedValuesDefinedAbove({loopOp.getRegion()}, preloopDefinedVals);
     // Insert values of forOp itself.
     llvm::for_each(loopOp.getOperands(),
-                   [&](Value forOpVal) { outDefinedVals.insert(forOpVal); });
-    return outDefinedVals;
+                   [&](Value forOpVal) { preloopDefinedVals.insert(forOpVal); });
+    return preloopDefinedVals;
   }
 
-  static llvm::SmallVector<Type>
-  getOutlineFuncInputsTypes(MLIRContext *ctx,
-                            const llvm::SetVector<Value> &outDefinedVals) {
-    llvm::SmallVector<Type> outlinedFuncInputTypes;
-    for (int i = 0; i < outDefinedVals.size(); i++) {
-      auto valType = outDefinedVals[i].getType();
-      auto valTypeInfo = inspectTypeInfo(valType);
-
-      if (matchPattern(outDefinedVals[i], m_Constant())) {
-        // DO NOTHING
-      } else if (llvm::isa<mlir::MemRefType>(valType)) {
-        outlinedFuncInputTypes.push_back(valType);
-      } else if (valType.isIntOrFloat()) {
-        outlinedFuncInputTypes.push_back(
-            MemRefType::get(valTypeInfo.shape, valTypeInfo.elementTy, {}, {}));
-      } else if (valType.isIndex()) {
-        outlinedFuncInputTypes.push_back(MemRefType::get(
-            valTypeInfo.shape, IntegerType::get(ctx, 64), {}, {}));
-      } else {
-        llvm::errs() << "Cannot handle this!\n";
-        outDefinedVals[i].dump();
-        std::exit(EXIT_FAILURE);
-      }
-    }
-    return outlinedFuncInputTypes;
-  }
-
-  static func::FuncOp
-  createOutlineFunc(func::FuncOp funcOp, LoopType loopOp, PatternRewriter &rewritter,
-                    const llvm::SmallVector<Type> &outlinedFuncInputTypes) {
+  static func::FuncOp createOutlineFunc(
+    func::FuncOp funcOp, 
+    LoopType loopOp, 
+    PatternRewriter &rewritter,
+    const llvm::SmallVector<Type> &outlinedFuncInputTypes,
+    const llvm::SmallVector<Type> &outlinedFuncOutputTypes
+  ) {
     rewritter.setInsertionPoint(funcOp);
-    auto outlinedFuncType =
-        rewritter.getFunctionType(outlinedFuncInputTypes, {});
+    auto outlinedFuncType = rewritter.getFunctionType(outlinedFuncInputTypes, outlinedFuncOutputTypes);
     auto outlinedFuncName = llvm::formatv("outlined_affinefor_{0}",
-                                          reinterpret_cast<std::uintptr_t>(
-                                              loopOp.getAsOpaquePointer()))
-                                .str();
+                                          reinterpret_cast<std::uintptr_t>(loopOp.getAsOpaquePointer())).str();
     llvm::SmallVector<NamedAttribute> attrs = {};
     llvm::SmallVector<DictionaryAttr> argAttrs = {};
-    auto outlinedFunc =
-        func::FuncOp::create(rewritter, funcOp.getLoc(), outlinedFuncName,
-                             outlinedFuncType, attrs, argAttrs);
+    auto outlinedFunc = func::FuncOp::create(rewritter, funcOp.getLoc(), outlinedFuncName, outlinedFuncType, attrs, argAttrs);
     return outlinedFunc;
   }
 
-  static void prepareCallOp(MLIRContext *ctx, LoopType loopOp,
-                            PatternRewriter &rewritter,
-                            llvm::SmallVector<Value> &realInputArgs,
-                            const llvm::SetVector<Value> &outDefinedVals) {
+  static void prepareCallOp(
+    MLIRContext *ctx, LoopType loopOp,
+    PatternRewriter &rewritter,
+    llvm::SmallVector<Value> &realInputArgs,
+    llvm::DenseMap<Value, Value> &affineResToMem,
+    const llvm::SetVector<Value> &preloopDefinedVals
+  ) {
     rewritter.setInsertionPoint(loopOp);
-    for (int i = 0; i < outDefinedVals.size(); i++) {
-      auto outVal = outDefinedVals[i];
-      if (matchPattern(outVal, m_Constant())) {
+    for (auto val: preloopDefinedVals) {
+      if (matchPattern(val, m_Constant())) {
         // DO NOTHING (constants are supposed to be already folded, so they will
         // not be used as arguments of outlined function). (Compute args will
         // not be folded in the outer function as constants, they will be
         // wrapped by memref usually).
-      } else if (llvm::isa<mlir::MemRefType>(outVal.getType())) {
-        realInputArgs.push_back(outVal);
-      } else if (outVal.getType().isIndex()) {
+      } else if (llvm::isa<mlir::MemRefType>(val.getType())) {
+        realInputArgs.push_back(val);
+      } else if (val.getType().isIndex()) {
         Type elementType = IntegerType::get(ctx, 32);
-        auto castOp = arith::IndexCastOp::create(rewritter, loopOp.getLoc(),
-                                                 elementType, outVal);
-        auto allocaOp =
-            memref::AllocaOp::create(rewritter, loopOp.getLoc(),
-                                     MemRefType::get({}, elementType, {}, {}));
-        auto storeOp = memref::StoreOp::create(rewritter, loopOp.getLoc(),
-                                               castOp.getResult(),
-                                               allocaOp.getResult(), {});
+        auto castOp = arith::IndexCastOp::create(rewritter, loopOp.getLoc(), elementType, val);
+        auto allocaOp = memref::AllocaOp::create(rewritter, loopOp.getLoc(), MemRefType::get({}, elementType, {}, {}));
+        auto storeOp = memref::StoreOp::create(rewritter, loopOp.getLoc(), castOp.getResult(), allocaOp.getResult(), {});
         realInputArgs.push_back(storeOp.getMemRef());
-      } else if (outVal.getType().isIntOrFloat()) {
-        auto allocaOp = memref::AllocaOp::create(
-            rewritter, loopOp.getLoc(),
-            MemRefType::get({}, outVal.getType(), {}, {}));
-        auto storeOp = memref::StoreOp::create(
-            rewritter, loopOp.getLoc(), outVal, allocaOp.getResult(), {});
+      } else if (val.getType().isIntOrFloat()) {
+        auto allocaOp = memref::AllocaOp::create(rewritter, loopOp.getLoc(), MemRefType::get({}, val.getType(), {}, {}));
+        auto storeOp = memref::StoreOp::create(rewritter, loopOp.getLoc(), val, allocaOp.getResult(), {});
         realInputArgs.push_back(storeOp.getMemRef());
       } else {
         llvm::errs() << "Unexpected Type!\n";
+        val.dump();
+        std::exit(EXIT_FAILURE);
+      }
+    }
+    for (auto val: loopOp.getResults()) {
+      if (val.getType().isIntOrFloat()) {
+        auto allocaOp = memref::AllocaOp::create(rewritter, loopOp.getLoc(), MemRefType::get({}, val.getType(), {}, {}));
+        realInputArgs.push_back(allocaOp.getMemref());
+        affineResToMem.insert(std::pair(val, allocaOp.getMemref()));
+      } else {
+        llvm::errs() << "Unexpected Type!\n";
+        val.dump();
         std::exit(EXIT_FAILURE);
       }
     }
@@ -141,13 +123,13 @@ struct AffineLoopsToOutline : public OpRewritePattern<LoopType> {
 
   static void fillOutlinedFunc(MLIRContext *ctx, LoopType loopOp,
                                PatternRewriter &rewritter, Block *entryBlock,
-                               const llvm::SetVector<Value> &outDefinedVals,
+                               const llvm::SetVector<Value> &preloopDefinedVals,
                                const llvm::SmallVector<Value> &realInputArgs) {
     rewritter.setInsertionPointToEnd(entryBlock);
     IRMapping mapping;
     int outlinedFuncIdx = 0;
-    for (int i = 0; i < outDefinedVals.size(); i++) {
-      auto outVal = outDefinedVals[i];
+    for (int i = 0; i < preloopDefinedVals.size(); i++) {
+      auto outVal = preloopDefinedVals[i];
       auto blockArg = entryBlock->getArgument(outlinedFuncIdx);
       outlinedFuncIdx += 1;
 
@@ -189,12 +171,35 @@ struct AffineLoopsToOutline : public OpRewritePattern<LoopType> {
         std::exit(EXIT_FAILURE);
       }
     }
+
     for (int i = 0; i < realInputArgs.size(); i++) {
       mapping.map(*(realInputArgs.begin() + i), entryBlock->getArgument(i));
     }
-    rewritter.clone(*loopOp.getOperation(), mapping);
-    func::ReturnOp::create(rewritter,
-                           loopOp.getLoc()); // the returnOp should be empty
+    auto cloned = rewritter.clone(*loopOp.getOperation(), mapping);
+    for (const auto& val: cloned->getResults()) {
+      auto blockArg = entryBlock->getArgument(outlinedFuncIdx);
+      affine::AffineStoreOp::create(rewritter, loopOp.getLoc(), val, blockArg, {});
+      outlinedFuncIdx += 1;
+    }
+    func::ReturnOp::create(rewritter, loopOp.getLoc()); // the returnOp should be empty
+  }
+
+  static Type convertToParamType(MLIRContext* ctx, Value val) {
+    auto valType = val.getType();
+    auto valTypeInfo = inspectTypeInfo(valType);
+    if (matchPattern(val, m_Constant())) {
+      return nullptr;
+    } else if (llvm::isa<mlir::MemRefType>(valType)) {
+      return valType;
+    } else if (valType.isIntOrFloat()) {
+      return MemRefType::get(valTypeInfo.shape, valTypeInfo.elementTy, {}, {});
+    } else if (valType.isIndex()) {
+      return MemRefType::get(valTypeInfo.shape, IntegerType::get(ctx, 64), {}, {});
+    } else {
+      llvm::errs() << "Cannot handle this!\n";
+      val.dump();
+      std::exit(EXIT_FAILURE);
+    }
   }
 
   LogicalResult matchAndRewrite(LoopType loopOp,
@@ -209,21 +214,43 @@ struct AffineLoopsToOutline : public OpRewritePattern<LoopType> {
       llvm::errs() << "loopOp should be wrapped in a function Op!\n";
       std::exit(EXIT_FAILURE);
     }
-
     auto ctx = rewritter.getContext();
-    auto outDefinedVals = getOutDefinedVals(loopOp);
-    auto outlinedFuncInputsTypes =
-        getOutlineFuncInputsTypes(ctx, outDefinedVals);
-    auto outlinedFunc =
-        createOutlineFunc(funcOp, loopOp, rewritter, outlinedFuncInputsTypes);
+
+    // Including defined values before the loop, loop operands.
+    llvm::SetVector<Value> dependVals;
+    mlir::getUsedValuesDefinedAbove({loopOp.getRegion()}, dependVals);
+    llvm::for_each(loopOp.getOperands(), [&](Value forOpVal) { dependVals.insert(forOpVal); });
+    auto loopReturnedVals  = loopOp.getResults();
+
+    llvm::SmallVector<Type> outlinedFuncInputTypes;
+    llvm::for_each(dependVals, [&](Value val){
+      auto ty = convertToParamType(ctx, val);
+      if (ty) {
+        outlinedFuncInputTypes.push_back(ty);
+      }
+    });
+    llvm::for_each(loopReturnedVals, [&](Value val){
+      auto ty = convertToParamType(ctx, val);
+      if (ty) {
+        outlinedFuncInputTypes.push_back(ty);
+      }
+    });
+    auto outlinedFunc = createOutlineFunc(funcOp, loopOp, rewritter, outlinedFuncInputTypes, {});
     auto entryBlock = outlinedFunc.addEntryBlock();
+
     llvm::SmallVector<Value> realInputArgs;
-    prepareCallOp(ctx, loopOp, rewritter, realInputArgs, outDefinedVals);
+    llvm::DenseMap<Value, Value> affineResToMem;
+    prepareCallOp(ctx, loopOp, rewritter, realInputArgs, affineResToMem, dependVals);
+    fillOutlinedFunc(ctx, loopOp, rewritter, entryBlock, dependVals, realInputArgs);
+
     rewritter.setInsertionPoint(loopOp);
-    func::CallOp::create(rewritter, loopOp.getLoc(), outlinedFunc,
-                         realInputArgs);
-    fillOutlinedFunc(ctx, loopOp, rewritter, entryBlock, outDefinedVals,
-                     realInputArgs);
+    func::CallOp::create(rewritter, loopOp.getLoc(), outlinedFunc, realInputArgs);
+    rewritter.setInsertionPointAfter(loopOp);
+    for (const auto& entry: affineResToMem) {
+      auto loadOp = affine::AffineLoadOp::create(rewritter, loopOp.getLoc(), entry.getSecond(), {});
+      rewritter.replaceAllUsesWith(entry.getFirst(), loadOp.getResult());
+    }
+
     rewritter.eraseOp(loopOp);
     return success();
   }
@@ -265,8 +292,6 @@ struct OutlineAffinePass
         return;
       }
     });
-    moduleOp.dump();
-
     return;
   }
 };
