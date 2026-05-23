9dcc57ad8a8609bd4ddd77011ceb1886f02ec867
Muyao Xiao <monetshaw@outlook.com>
2026-05-23 17:41:10 +0900

    [pass] remerge pass

--- src/transform/RemergePass.cpp
+++ src/transform/RemergePass.cpp
@@ -4,7 +4,9 @@
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/Value.h"
 #include "mlir/Pass/Pass.h"
+#include <cassert>
 #include <cstdlib>
 #include <regex>
 #include <string>
@@ -15,7 +17,6 @@
 #include "mlir/Support/WalkResult.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "llvm/ADT/DenseMap.h"
-#include "llvm/ADT/DenseSet.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/FormatVariadic.h"
@@ -40,6 +41,51 @@ static std::string getCorrespondingStableHLOFuncName(::mlir::StringRef outlinedF
 
 struct ReplaceOutlineFuncCall : public OpRewritePattern<func::FuncOp> {
   using OpRewritePattern::OpRewritePattern;
+
+  static void replaceMemrefFuncCall(
+    ModuleOp moduleOp,
+    PatternRewriter &rewritter,
+    func::CallOp callOp, 
+    ::mlir::StringRef outlinedFuncName
+  ) {
+    func::FuncOp outlinedFunc = moduleOp.lookupSymbol<func::FuncOp>(outlinedFuncName);
+    auto hloFuncName = llvm::formatv("{0}_raised", outlinedFuncName).str(); 
+    func::FuncOp hloFunc = moduleOp.lookupSymbol<func::FuncOp>(hloFuncName);
+    assert(outlinedFunc.getNumArguments() == hloFunc.getNumArguments());
+    assert(outlinedFunc.getNumResults() == 0);
+    assert(hloFunc.getNumArguments() == hloFunc.getNumResults());
+
+    rewritter.setInsertionPoint(callOp);
+    auto memrefFuncParams = callOp.getOperands();
+    int paramsSize = memrefFuncParams.size(); 
+    assert(paramsSize == outlinedFunc.getNumArguments());
+
+    llvm::SmallVector<Value> tensorParams;
+    for (int paramIdx = 0; paramIdx < paramsSize; paramIdx++) {
+      auto memrefFuncParam = memrefFuncParams[paramIdx];
+      MemRefType operandMemrefType = llvm::dyn_cast<MemRefType>(memrefFuncParam.getType());
+      auto operandTensorType = RankedTensorType::get(operandMemrefType.getShape(), operandMemrefType.getElementType());
+      auto memrefTypeToTensorTypeOp = bufferization::ToTensorOp::create(rewritter, callOp.getLoc(), operandTensorType, memrefFuncParam);
+      tensorParams.push_back(memrefTypeToTensorTypeOp);
+    }
+    auto hloFuncCallOp = func::CallOp::create(rewritter, callOp.getLoc(), hloFunc, tensorParams);
+    
+    rewritter.setInsertionPointAfter(callOp);
+    for (int paramIdx = 0; paramIdx < paramsSize; paramIdx++) {
+      auto hloFuncCallOpRes = hloFuncCallOp.getResult(paramIdx);
+      RankedTensorType hloResultTensorType = llvm::dyn_cast<RankedTensorType>(hloFuncCallOpRes.getType()); 
+      auto hloResultMemrefType = MemRefType::get(hloResultTensorType.getShape(), hloResultTensorType.getElementType());
+      auto hloTensorResToMemrefOp = bufferization::ToBufferOp::create(rewritter, callOp.getLoc(), hloResultMemrefType, hloFuncCallOpRes);
+      rewritter.replaceUsesWithIf(memrefFuncParams[paramIdx], hloTensorResToMemrefOp.getResult(), [&](OpOperand& useOperand){
+        auto useOp = useOperand.getOwner();
+        return hloTensorResToMemrefOp.getResult().getDefiningOp()->isBeforeInBlock(useOp);
+      });
+    }
+    
+    rewritter.eraseOp(callOp);
+    return;
+  }
+
 
   LogicalResult matchAndRewrite(func::FuncOp funcOp,
                                 PatternRewriter &rewritter) const final {
@@ -66,15 +112,7 @@ struct ReplaceOutlineFuncCall : public OpRewritePattern<func::FuncOp> {
     for (const auto& entry: callToOutlineFuncMap) {
       auto callOp = entry.getFirst();     
       auto outlinedFuncName = entry.getSecond();
-
-      func::FuncOp outlinedFunc = moduleOp.lookupSymbol<func::FuncOp>(outlinedFuncName);
-      rewritter.setInsertionPoint(callOp);
-      auto operands = callOp.getOperands();
-      llvm::SmallVector<Value> tensorParams;
-      for (const auto& operand: operands) {
-        Value memrefParam = llvm::DynCastTo<MemRefType>(operand);
-        bufferization::ToTensorOp::create(rewritter, callOp.getLoc(), TensorType(), operand);
-      }
+      replaceMemrefFuncCall(moduleOp, rewritter, callOp, outlinedFuncName);
     };
 
     return failure();
@@ -84,6 +122,7 @@ struct ReplaceOutlineFuncCall : public OpRewritePattern<func::FuncOp> {
 
 struct RemergePass: public mlir::PassWrapper<RemergePass, OperationPass<ModuleOp>> {
   void getDependentDialects(mlir::DialectRegistry & registry) const override {
+    registry.insert<bufferization::BufferizationDialect>();
   }
 
   StringRef getArgument() const override { 
